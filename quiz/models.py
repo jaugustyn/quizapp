5762ea94b0442ed8f8e276ab6683a8ce61454200
jaugustyn <kub4a1@gmail.com>
2021-09-13 16:08:42 +0200

    Category model

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=25, unique=True, null=False)
+    image = models.ImageField(upload_to='quiz/static/images/category_images/', null=False, default="")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
