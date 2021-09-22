baef6cb0c370c6485dea36c84ee0d7ffa8969692
jaugustyn <kub4a1@gmail.com>
2021-09-22 19:19:15 +0200

    CRUD added to category

--- quiz/urls.py
+++ quiz/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 from rest_framework.schemas import get_schema_view
 
 urlpatterns = [
-    path('docs/', TemplateView.as_view(
+    path('', TemplateView.as_view(
         template_name='swagger-ui.html',
         extra_context={'schema_url': 'openapi-schema'}
     ), name='swagger-ui'),
@@ -14,7 +14,8 @@ urlpatterns = [
         version="1.0.0"
     ), name='openapi-schema'),
 
-    path('', views.api_overview, name="api-overview"),  # Api overview
+    # path('', views.api_overview, name="api-overview"),  # Api overview
     path('categories/', views.category_list.as_view(), name="category_list"),
-    path('categories/<str:slug>/', views.quiz_list, name="quiz_list"),
+    path('categories/<str:name>/', views.category_update.as_view(), name="category_update"),
+    path('questions/<str:slug>/', views.quiz_list, name="quiz_list"),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -15,14 +15,31 @@ def api_overview(request):
     api_urls = {
         'Api Overview': "/",
         'Category List': 'categories/',
-        'Quiz Detail': 'categories/<category_name>/',
+        'Quiz Detail': 'questions/<category_name>/',
     }
     return Response(api_urls)
 
 
-class category_list(generics.ListAPIView):
+class category_list(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+
+class category_update(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = CategorySerializer
+    lookup_url_kwarg = "name"
+    lookup_field = "name"
+
+
+# @api_view(['GET', 'POST', "PUT", "DELETE"])
+# def category_crud(request, pk):
+#     quiz = Category.objects.get(pk=pk)
+#     serializer = CategorySerializer(instance=quiz, data=request.data)
+#
+#     if serializer.is_valid():
+#         serializer.save()
+#         return Response(serializer.data)
+#     return Response(serializer.errors)
 
 
 @api_view(['GET'])
@@ -34,12 +51,4 @@ def quiz_list(request, slug):
         return Response(serializer.data)
 
 
-# @api_view(['POST'])
-# def category_update(request, pk):
-#     quiz = Category.objects.get(pk=pk)
-#     serializer = CategorySerializer(instance=quiz, data=request.data)
-#
-#     if serializer.is_valid():
-#         serializer.save()
-#         return Response(serializer.data)
-#     return Response(serializer.errors)
+
