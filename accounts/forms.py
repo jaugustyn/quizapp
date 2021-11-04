46808fed016cce6ab6ccdaed5d5a5822a6f87023
jaugustyn <kub4a1@gmail.com>
2021-11-04 17:39:25 +0100

    Visibility fix

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,24 +5,25 @@ from django.core.validators import validate_email
 from .views import User
 
 
-# class RegistrationForm(forms.ModelForm):
-#     first_name = forms.CharField(max_length=50)
-#     last_name = forms.CharField(max_length=100)
-#     birth_date = forms.DateField(input_formats='%d-%m-%Y',
-#                                  widget=django.forms.DateInput(attrs={'placeholder': "__-__-____"}))
-#     email = forms.EmailField(max_length=100, validators=[validate_email])
-#     password = forms.CharField(max_length=50, widget=forms.PasswordInput)
-#
-#     class Meta:
-#         model = User
-#         fields = ['first_name', 'last_name', 'birth_date', 'email', 'password']
-#         widgets = {
-#             'password': forms.PasswordInput(),
-#         }
-#
-#     def save(self, commit=True):
-#         user = super().save(commit=False)
-#         user.set_password(self.cleaned_data['password'])
-#         if commit:
-#             user.save()
-#         return user
+# Not used at the moment
+class RegistrationForm(forms.ModelForm):
+    first_name = forms.CharField(max_length=50)
+    last_name = forms.CharField(max_length=100)
+    birth_date = forms.DateField(input_formats='%d-%m-%Y',
+                                 widget=django.forms.DateInput(attrs={'placeholder': "__-__-____"}))
+    email = forms.EmailField(max_length=100, validators=[validate_email])
+    password = forms.CharField(max_length=50, widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'birth_date', 'email', 'password']
+        widgets = {
+            'password': forms.PasswordInput(),
+        }
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+        return user
