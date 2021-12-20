bd55e25ee9c68867d12d39ff81e090fdaf50ae13
jaugustyn <kub4a1@gmail.com>
2021-12-20 22:39:11 +0100

    Updated allowed hosts in settings

--- website/settings.py
+++ website/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://quiz-app-restapi.herokuapp.com/']
+ALLOWED_HOSTS = ['https://quiz-app-restapi.herokuapp.com']
 
 # Application definition
 
